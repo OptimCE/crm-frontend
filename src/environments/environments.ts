5908da7c7b4cc8daeb93f234c30378169c3fed4b
EricPaque <60603143+Radisio@users.noreply.github.com>
2026-02-13 08:42:21 +0100

    Add `environments` file with local configuration

---
+++ src/environments/environments.ts
@@ -0,0 +1,10 @@
+export const environments = {
+  apiUrl: 'http://localhost:8080',
+  basePath: 'http://localhost:4200',
+  keycloak:{
+    realm : 'optimce-realm',
+    url: 'http://localhost:8081',
+    clientId: 'optimce-frontend',
+    urlPattern: /^(http:\/\/localhost:8080)(\/.*)?$/i
+  }
+};
