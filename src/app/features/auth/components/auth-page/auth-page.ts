84f5629ebd6a23254b0417a55bd5112e64c786eb
EricPaque <60603143+Radisio@users.noreply.github.com>
2026-03-24 15:11:53 +0100

    Add new SVG asset `members.svg` for slider images in the project.

--- src/app/features/auth/components/auth-page/auth-page.ts
+++ src/app/features/auth/components/auth-page/auth-page.ts
@@ -4,16 +4,18 @@ import { Carousel } from 'primeng/carousel';
 import { TranslatePipe } from '@ngx-translate/core';
 import { LanguageSelector } from '../../../../shared/components/language-selector/language-selector';
 import Keycloak from 'keycloak-js';
+import { NgOptimizedImage } from '@angular/common';
 
 interface Slide {
   icon: string;
   titleKey: string;
   descKey: string;
+  images: string;
 }
 
 @Component({
   selector: 'app-auth-page',
-  imports: [Button, Carousel, TranslatePipe, LanguageSelector],
+  imports: [Button, Carousel, TranslatePipe, LanguageSelector, NgOptimizedImage],
   standalone: true,
   templateUrl: './auth-page.html',
   styleUrl: './auth-page.css',
@@ -26,21 +28,25 @@ export class AuthPage {
       icon: 'pi-users',
       titleKey: 'LOGIN.SLIDES.MEMBERS_TITLE',
       descKey: 'LOGIN.SLIDES.MEMBERS_DESC',
+      images: 'assets/images/sliders/members',
     },
     {
       icon: 'pi-bolt',
       titleKey: 'LOGIN.SLIDES.METERS_TITLE',
       descKey: 'LOGIN.SLIDES.METERS_DESC',
+      images: 'assets/images/sliders/chart',
     },
     {
       icon: 'pi-share-alt',
       titleKey: 'LOGIN.SLIDES.SHARING_TITLE',
       descKey: 'LOGIN.SLIDES.SHARING_DESC',
+      images: 'assets/images/sliders/sharing',
     },
     {
       icon: 'pi-key',
       titleKey: 'LOGIN.SLIDES.KEYS_TITLE',
       descKey: 'LOGIN.SLIDES.KEYS_DESC',
+      images: 'assets/images/sliders/sun',
     },
   ];
 
